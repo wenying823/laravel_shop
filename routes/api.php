01d06a2e5065c3e200e2c68b8f31680e1322e0f5
Todd <todd@playmore.com.hk>
2025-01-06 14:30:24 +0800

    [feat]product api design

---
+++ routes/api.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\Api\ProductController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']);
+
